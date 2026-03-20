e34b72156154a8798f4bc6703a329f3551db46eb
Suerine Otieno <suerineogaja@gmail.com>
2026-03-20 12:24:02 +0300

    undoing vercel changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://gof-ecommerce-website.onrender.com",
+  baseURL: "https://gof-ecommerce-website.onrender.com",
   withCredentials: true,
 });
 
